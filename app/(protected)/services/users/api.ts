cdca5bf4ec7ee574b4c42d863c91681635e862b3
durgeshv3cars <durgesh@v3cars.com>
2025-05-21 18:04:01 +0530

    Live code

--- app/(protected)/services/users/api.ts
+++ app/(protected)/services/users/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api";
 
+console.log("Home", API_BASE_URL)
 
 export const fetchUsers = async () => {
   try {
