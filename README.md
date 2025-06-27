30df107c3554b4f502bf98af68f94aeaabf40a25
durgeshv3cars <durgesh@v3cars.com>
2025-06-27 16:17:21 +0530

    udpate

--- README.md
+++
@@ -1,2 +0,0 @@
-"# v3carfrontend" 
-"# v3carfrontend" 
--- model/Brand.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const brandSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  country: { type: String, required: true },
-  foundedYear: { type: Number, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Brand', brandSchema);
